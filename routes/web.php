e615a106e568a2506ecf9a90f1b0fe4770eb65f9
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-03-27 23:16:07 +0700

    feat: redirect root route to /kasir instead of displaying welcome view

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\SaleThermalPrintController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/kasir');
 });
 
 Route::middleware(['auth'])->group(function () {
